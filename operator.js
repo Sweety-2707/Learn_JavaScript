53bacc344e65aecad3234137118b8b600e319e0a
Sweety Sharma <sweety2020727@gmail.com>
2023-12-30 18:53:17 +0530

    Create operator.js
    Operators used in JavaScript

---
+++ operator.js
@@ -0,0 +1,36 @@
+//Operators in JS
+let a=12;
+let b=5;
+
+//arithmetic operators
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+
+
+//assignment operators
+console.log("a+=",a+=5);
+console.log("a-=",a-=5);
+console.log("a*=",a*=5);
+console.log("a/=",a/=5);
+console.log("a**=",a**=5);
+
+let c=12;
+let d="12";
+//comparison operators
+console.log("d==c",d==c);   //compare only value
+console.log("d!=c",d!=c);
+console.log("d===c",d===c);   //compare value as well as type
+console.log("d!==c",d!==c);
+console.log("a>c",a>c);
+console.log("a<c",a<c);
+console.log("a>=c",a>=c);
+console.log("a<=c",a<=c);
+
+//logical operators
+console.log("a==b and a==c",a==b && d==c);  //and operator
+console.log("a==b or a==c",a==b || d==c);   //o operator
+console.log("!true",!true);   //not operator
