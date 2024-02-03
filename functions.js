186af24691f25d2d4aacf167226a619ce34155d6
Sweety Sharma <sweety2020727@gmail.com>
2024-02-03 23:51:11 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,15 @@
+//creating function
+let sum=(a,b)=>{
+    console.log("Sum of "+a+" and "+b+" is:")
+    return a+b
+}
+
+let ans=sum(2,3)   //function call return value assigned to ans
+console.log(ans)
+
+//creating function
+function hello(){
+    console.log("Hello Everyone")
+}
+//function call
+hello();
