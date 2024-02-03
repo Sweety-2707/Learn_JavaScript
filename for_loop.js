5c3271238e69f3b9eff3a9b71aeabd854d93f3b6
Sweety Sharma <sweety2020727@gmail.com>
2024-02-03 23:21:01 +0530

    Create for_loop.js

---
+++ for_loop.js
@@ -0,0 +1,19 @@
+//for loop
+for(let i=0;i<10;i++){
+    console.log(i+1);
+}
+
+let obj={
+    sweety:76,
+    saloni:80,
+    sonam:78
+}
+//for-in loop
+for(let o in obj){
+    console.log("Marks of "+o+ " is: "+obj[o]);
+}
+
+//for-of loop
+for(let i of "Object"){
+    console.log(i);
+}
