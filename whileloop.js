fbec9586be1e5b73402d4edc26c03533d3f7f374
Sweety Sharma <sweety2020727@gmail.com>
2024-02-03 23:35:29 +0530

    Create whileloop.js

---
+++ whileloop.js
@@ -0,0 +1,17 @@
+let i=1;
+
+//while loop
+//execute if and only if condition is true
+console.log("while loop")
+while(i<10){
+    console.log(i)
+    i++
+}
+
+//do while loop
+//execute atleast one time and then condition is checked
+console.log("do while loop")
+do{
+    console.log(i)
+    i++
+}while(i<10)
