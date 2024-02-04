33f8f01e13982e9084645e96dc4aa5b5f308cb19
Sweety Sharma <sweety2020727@gmail.com>
2024-02-04 23:52:35 +0530

    Create string_methods.js

---
+++ string_methods.js
@@ -0,0 +1,16 @@
+let name="Sweety"
+console.log(name.length)   //length method
+console.log(name.toUpperCase())  ///uppercase method
+console.log(name.toLowerCase())  //lowercase method
+console.log(name.slice(2,5))   //return char from index 2 to index 4 (5 not included)
+console.log(name.slice(3))  //return char from index 3 to end
+console.log(name.replace("ty","tie"))
+ 
+
+let name2="   Shivika    "
+console.log(name2)
+console.log(name2.length)
+console.log(name2.trim())  //remove space
+console.log(name2.trim().length)  
+console.log(name.concat(" and ",name2))  //concatenate two strings
+console.log(name+" and"+name2)  //concatenate strings
